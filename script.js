9762e376a2bb01795ee5c093d517a8bc7cbb09c4
Carlos Reges <kerg23@gmail.com>
2022-06-19 11:44:31 -0300

    Update script.js
    Sigo arreglando. Aquí lo convertí en función y la invoqué,

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
-    for(var i = 1 ; i<= 6;i++){
-        for (var j  = 1; j <= i; j++) {
+function triangle() {
+    for(let i = 1 ; i<= 6;i++){
+        for (let j  = 1; j <= i; j++) {
             document.write(i);
+            console.log(i);
         }
         document.write("<br>");
     }
+}
+
+triangle();
